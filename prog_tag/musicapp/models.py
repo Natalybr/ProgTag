723677bac5d0d17b1f77eaaf31cef75b49313ab0
arielbro <arielbro@mail.tau.ac.il>
2013-12-19 16:15:23 +0200

    Created a skeleton for some urls.
    P.S. Natalie, you are special and forever holds a place in my heart.

--- prog_tag/musicapp/models.py
+++ prog_tag/musicapp/models.py
@@ -2,11 +2,12 @@ from django.db import models
 
 # Create your models here.
 
-
 class Tag(models.Model):
-    tag_name = models.CharField(max_length=200)
-
+    tag_name = models.CharField(max_length = 200)
+    
 class Song(models.Model):
+    
+    def __repr__(self):
+        return self.song_name
     song_name = models.CharField(max_length=200)
     song_tag = models.ForeignKey(Tag)
-
