46e31a5835cd87568ef646cd008034409f0a3b33
arielbro <arielbro@mail.tau.ac.il>
2014-02-10 20:47:54 +0200

    Perhaps this is the conflict resolve!

--- prog_tag/musicapp/server/cool_stuff.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from musicapp.models import *
-
-def foo(request):
-    response = HttpResponse("Here is some text!")
-    response.write("</br> haha!")
-
-    return response
